dbf2dc5de8242c2d21e89a33c16b44f6b9787cc9
Adrian Ciucanel <adrian.ciucanel@endava.com>
2020-03-20 17:20:54 +0200

    Add argo hook

--- tcon/TCON20-579/bulk_scripts/watch.py
+++ tcon/TCON20-579/bulk_scripts/watch.py
@@ -4,12 +4,9 @@ from kubernetes import client, config, watch
 config.load_kube_config()
 
 v1 = client.CoreV1Api()
-count = 10
 w = watch.Watch()
 for event in w.stream(v1.list_namespace, _request_timeout=600):
-    print("Event: %s %s %s" % (event['type'], event['object'].metadata.name, event['object'].metadata.typ))
-    count -= 1
-    if not count:
-        w.stop()
+    print("Event: %s %s " % (event['type'], event['object'].metadata.name))
+    # print(event)
 
 print("Ended.")
