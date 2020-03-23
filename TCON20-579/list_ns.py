92f331e50aad7ab7f23fb0d7f5c2c68920c6eb89
Andrei Bamboi <Andrei.Bamboi@endava.com>
2020-03-23 13:50:43 +0200

    update

--- TCON20-579/list_ns.py
+++ TCON20-579/list_ns.py
@@ -1,3 +1,16 @@
 from kubernetes import client, config, watch
+import subprocess
+
 def main():
-    config.load_kube_config()
+    # config.load_incluster_config()
+    # config.load_kube_config()
+    # api = client.CoreV1Api()
+    # ns = api.list_namespace(pretty=True)
+    # print(ns)
+    res = subprocess.check_output(["kubectl", "get", "ns"])
+    for line in res.splitlines():
+        print(line)
+
+# process the output line by line
+
+main()
