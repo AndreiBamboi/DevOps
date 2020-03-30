c2d738f11b572d5898b66335f2772fe755ceb99b
Andrei Bamboi <Andrei.Bamboi@endava.com>
2020-03-30 13:33:20 +0300

    update

--- tcon/TCON20-579/list_ns.py
+++
@@ -1,9 +0,0 @@
-from kubernetes import client, config, watch
-import subprocess
-
-def main():
-    config.load_kube_config()
-    api = client.CoreV1Api()
-    ns = api.list_namespace()
-    print(ns)
-main()
