85f286fdc50a0e95a9f6a5ac0f954dd923726f2c
Andrei Bamboi <andrei.bamboi@endava.com>
2020-05-14 12:22:17 +0300

    update

---
+++ k8s-tools/cpu_req_per_container.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+kubectl get po --all-namespaces -o=jsonpath="{range .items[*]}{.metadata.namespace}:{.metadata.name}{'\n'}{range .spec.containers[*]}  {.name}:{.resources.requests.cpu}{'\n'}{end}{'\n'}{end}"
